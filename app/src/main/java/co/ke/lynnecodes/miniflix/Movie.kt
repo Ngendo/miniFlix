66ad77b7804e082725cee4c450c5d7657dcad88a
Lydiah Wanjiru <ngendo.lynne@gmail.com>
2021-02-23 13:22:04 +0300

    Added movies data class

---
+++ app/src/main/java/co/ke/lynnecodes/miniflix/Movie.kt
@@ -0,0 +1,23 @@
+package co.ke.lynnecodes.miniflix
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by Lynne on 23,February,2021
+ */
+data class Movie(
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("vote_average")
+    val rating: Float,
+    @SerializedName("release_date")
+    val releaseDate: String
+)
